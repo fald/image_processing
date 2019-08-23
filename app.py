b6b94d13f4636bff07d04fb98bed37be5771f527
Firas <firasaldabbas+git@gmail.com>
2019-08-22 21:46:22 -0400

    kernels in place

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ import cv2
 
 app_name = "m'app"
 file_name = "test.png"
-# TODO: Change this from trackbar. Maybe 1 trackbar per filter, I dunno. Or subwindows with more options.
-# This is a stupid way to do it, but tutorials.
-num_filters = 1
 
 def dummy(value):
-    pass
+    print(value)
+
+# TODO: Change this from trackbar. Maybe 1 trackbar per filter, I dunno. Or subwindows with more options.
+# This is a stupid way to do it, but tutorials.
+# Define our convolution kernels.
+# Identity, doesn't change anything at all.
+identity_kernel = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+# A 5 surrounded by -1s; most weight on anchor point, less weight on surrounding.
+sharpen_kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+gaussian_kernel_1 = cv2.getGaussianKernel(3, 0)
+gaussian_kernel_2 = cv2.getGaussianKernel(9, 0)
+# Averaging kernel, this is actually good for me!
+box_size = 55
+box_kernel = np.ones((box_size, box_size), dtype=np.float32) / float(box_size ** 2)
+
+kernels = [identity_kernel, sharpen_kernel, gaussian_kernel_1, gaussian_kernel_2, box_kernel]
 
 # Read in an  image, then make it grayscale
 # TODO: File selecting
@@ -29,8 +41,7 @@ cv2.namedWindow(app_name)
 cv2.createTrackbar('Contrast', app_name, 1, 100, dummy)
 cv2.createTrackbar('Brightness', app_name, 50, 100, dummy)
 cv2.createTrackbar('Grayscale', app_name, 0, 1, dummy) # TODO: Grayscale scaling 0-255?
-cv2.createTrackbar("Filters", app_name, 0, num_filters, dummy)
-
+cv2.createTrackbar("Filters", app_name, 0, len(kernels) - 1, dummy)
 #
 # MAIN UI LOOP
 #
@@ -39,15 +50,19 @@ while True:
     contrast = cv2.getTrackbarPos("Contrast", app_name)
     brightness = cv2.getTrackbarPos("Brightness", app_name)
     filters = cv2.getTrackbarPos("Filters", app_name)
+    kernel_index = cv2.getTrackbarPos('Filter', app_name)
     # TODO: Apply filters
-    # TODO: Apply brightness/contrast
+    color_modified = cv2.filter2D(color_original, -1, kernels[kernel_index])
+    gray_modified = cv2.filter2D(gray_original, -1, kernels[kernel_index])
     # addWeighted needs a second useless image, for some reason.
     # The way this is working is we're adding the original to a dummy, then scaling it; its the
     # scaling that does the brightness, so no need for a second image, but the method in cv2 still
     # requires it.
     # Remember to take into account brightness starting at 50 on the tackbar.
-    color_modified = cv2.addWeighted(color_original, contrast, np.zeros_like(color_original), 0, brightness - 50)
-    gray_modified = cv2.addWeighted(gray_original, contrast, np.zeros_like(gray_original), 0, brightness - 50)
+    # color_modified = cv2.addWeighted(color_original, contrast, np.zeros_like(color_original), 0, brightness - 50)
+    # gray_modified = cv2.addWeighted(gray_original, contrast, np.zeros_like(gray_original), 0, brightness - 50)
+    color_modified = cv2.addWeighted(color_modified, contrast, np.zeros_like(color_original), 0, brightness - 50)
+    gray_modified = cv2.addWeighted(gray_modified, contrast, np.zeros_like(gray_original), 0, brightness - 50)
     # TODO: Buttons to open/save/close
 
     # wait for keypress, this seems like a shit way, just use buttons at this point
